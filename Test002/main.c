cfaa824ff4b24dbf8e0b1b59e991646ebb2670de
marlonX88 <805992059@qq.com>
2014-11-13 21:34:03 +0800

    Initial Commit

---
+++ Test002/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Test002
+//
+//  Created by 王彪 on 11/13/14.
+//  Copyright (c) 2014 Apress. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
